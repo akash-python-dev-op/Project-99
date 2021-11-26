adecfd7434fcdea166c00ff6c696135958f333cf
Kabir1402 <71212806+Kabir1402@users.noreply.github.com>
2021-11-26 17:03:32 +0530

    Add files via upload

---
+++ Pro-C99/removeFiles.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+import time
+
+path = 'D:\Others\Pro-C99\removeFiles.py'
+days = 10
+
+desiredTime = time.time(days)
+os.path.exists.path(path)
+os.walk(path)
+os.path.join(removeFiles.py,path)
+ctime = os.stat(path).st_ctime
+
+if ctime>desiredTime:
+    os.remove(path)
